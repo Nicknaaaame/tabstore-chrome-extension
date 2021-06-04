9b575f9d47045e541b7d95f12a6789afe741217a
Danil <dlyaregistr123@bk.ru>
2021-06-04 22:00:36 +0700

    task is completed

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,1 @@
-import {devCons} from './environments/environment';
-
-devCons.log('Store is clean');
-chrome.storage.local.clear();
-chrome.storage.onChanged.addListener(changes => {
-  devCons.log('bg:', changes);
-});
 
